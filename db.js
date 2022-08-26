425e1fb8c7dd53108c90b475bdefd7369c38b0dd
Prantosh <bepariprantosh360@gmail.com>
2022-08-26 22:00:29 +0600

    first commit

---
+++ db.js
@@ -0,0 +1,13 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const players = require("./data/players");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  videos2,
+  players,
+  transactions,
+};
