f671f8840d10f271250d434c7cce163d41401483
Prantosh <bepariprantosh360@gmail.com>
2022-11-06 14:35:25 +0600

    add users.json

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+const users = require("./data/users");
 const todos = require("./data/todos");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
@@ -5,6 +6,7 @@ const players = require("./data/players");
 const transactions = require("./data/transactions");
 
 module.exports = {
+  users,
   todos,
   videos,
   videos2,
